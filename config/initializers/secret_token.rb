5c7e070948d2a373e77590ed54636c6e14c923cc
Danigb <danigb@gmail.com>
2011-05-23 19:42:29 +0200

    Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookcamp::Application.config.secret_token = 'a4cbbcadce65a1f45283eda1fee421a46b7b595998713582ec2fcaa74cc744ec83d23752ba6e17ef7eac34d98c32286031f0f743823f088ed6c326e547ae51b2'
