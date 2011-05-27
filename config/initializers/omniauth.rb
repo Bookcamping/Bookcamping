6d12e79c504da9ee3cc152c7e60e2e0f4b43a0d6
Danigb <danigb@gmail.com>
2011-05-27 16:42:57 +0200

    google auth support

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :openid, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
   provider :twitter, 'uUYLczFcet3Wopg3N6RGA', 'd77GrnYHiQrVDm60zBVquidH2BX7EinyO2NltkhY'
 end
 
