f2259228e826903b51dc01affff745e21c22d90b
Danigb <danigb@gmail.com>
2011-07-03 14:25:32 +0200

    bigger video logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,20 +11,7 @@ module ApplicationHelper
     content_tag(:div, Redcarpet.new(text, *options).to_html.html_safe, :class => 'markdown')
   end
 
-  def lists_div(size, &block)
-    content_tag(:div, content_tag(:div, :class => "lists", :style => "width: #{size * 380}px;", &block), :class => 'container')
-  end
-
-  def render_info
-    render 'layouts/intro'
-  end
-
-  def iconic(symbol, label = '')
-    content_tag(:span, '', :class => "iconic #{symbol}") + label.to_s
-  end
-
   def current_model
     current_camp.model_name
   end
-
 end
