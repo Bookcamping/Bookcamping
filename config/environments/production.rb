33987887559d6151fbb5693db34450093380ea0a
Danigb <danigb@gmail.com>
2011-09-12 13:51:06 +0200

    New relic. Some R3.1 default params

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,9 +12,14 @@ Bookcamp::Application.configure do
   config.serve_static_assets = false
   #config.serve_static_assets = true
 
-  # Compress both stylesheets and JavaScripts
-  config.assets.js_compressor  = :uglifier
-  # config.assets.css_compressor = :scss
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # Specifies the header that your server uses for sending files
   # (comment out if your front-end server doesn't support this)
