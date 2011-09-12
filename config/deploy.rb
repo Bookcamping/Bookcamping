1ac0ac093e31f225ca1669bdb6b56fd3dcbd62df
Danigb <danigb@gmail.com>
2011-09-12 14:08:38 +0200

    Add fallback to precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ namespace :deploy do
 end
 
 after 'deploy:update_code' do
-  run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
+#  run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
 end
 
 namespace :mysql do
