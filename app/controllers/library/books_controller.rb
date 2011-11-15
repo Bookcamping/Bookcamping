d28c189ffd8df840efc5b9c816add3fbc805e036
Danigb <danigb@gmail.com>
2011-11-15 09:23:55 +0100

    Uservoice feedback

--- app/controllers/library/books_controller.rb
+++ app/controllers/library/books_controller.rb
@@ -2,9 +2,10 @@
 class Library::BooksController < Shared::BooksController
   expose(:camp_shelf) { current_camp.camp_shelves.find params[:camp_shelf_id] }
   expose(:shelf) { camp_shelf }
+  expose(:pajax?) { request.headers['X-PJAX'].present? }
 
   def show
-    if request.headers['X-PJAX']
+    if pajax?
       render action: 'show_pjax', layout: false
     end
   end
