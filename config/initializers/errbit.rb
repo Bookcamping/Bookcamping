dbf84c63f07c03883db9b5b99a9685bff30c0091
danigb <danigb@gmail.com>
2011-11-20 01:49:17 +0100

    Ya me vale. Syntax error en errbit.rb

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,6 @@
 HoptoadNotifier.configure do |config|
-    config.api_key     = 'b32f5945dac5b4b66e3ae4c78c796f24'
-      config.host        = 'errcamping.herokuapp.com'
-      config.port        = 80
-      config.secure      = config.port == 443
-    end
+  config.api_key     = 'b32f5945dac5b4b66e3ae4c78c796f24'
+  config.host        = 'errcamping.herokuapp.com'
+  config.port        = 80
+  config.secure      = config.port == 443
 end
