bb4fb3ff4eadc1f91143338891d33fc732fb0222
danigb <danigb@gmail.com>
2011-11-21 11:37:12 +0100

    Library books controller bug

--- app/controllers/library/books_controller.rb
+++ app/controllers/library/books_controller.rb
@@ -1,6 +1,6 @@
 # Controller for Books belonging to CampShelves
 class Library::BooksController < Shared::BooksController
-  expose(:camp_shelf) { CampShelf.find_by_slug params[:camp_shelf_id] || CampShelf.find(params[:camp_shelf_id]) }
+  expose(:camp_shelf) { CampShelf.find_by_slug(params[:camp_shelf_id]) || CampShelf.find(params[:camp_shelf_id]) }
   expose(:shelf) { camp_shelf }
   expose(:pajax?) { request.headers['X-PJAX'].present? }
   expose(:current_camp) { camp_shelf.camp }
