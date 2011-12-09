83682d095ebf4daf79f568c9baf61a106a2a37ad
danigb <danigb@gmail.com>
2011-12-09 13:24:44 +0100

    Remove 'No se que licencia' from new reference form

---
+++ spec/requests/references/add_reference_feature.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+require 'spec_helper'
+
+feature 'add reference' do
+  background do
+    FactoryGirl.create(:camp)
+    @user = FactoryGirl.create(:user)
+    visit enter_path(@user.id)
+  end
+
+  scenario 'add a reference' do
+    visit new_book_path
+    puts page.body
+    page.should have_content('Dominio público')
+  end
+end
+
