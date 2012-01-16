6e529c0aa5aeddb3e30fc3a54c6000ed835f2672
danigb <danigb@gmail.com>
2012-01-16 16:50:31 +0100

    Presenters use the new redcarpet renderer

--- app/presenters/application_presenter.rb
+++ app/presenters/application_presenter.rb
@@ -3,12 +3,17 @@ class ApplicationPresenter < BasePresenter
     text.html_safe
   end
 
+  def renderer
+    options = {hard_wrap: true, autolink: true, fenced_code_blocks: true }
+    @renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+  end
+
   def markdown(text, limit = nil)
     text = '' if text.blank?
     text = truncate(text, :length => limit) if limit.present?
     # REMOVED_OPTIONS: :filter_html,
     options = [:hard_wrap, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
-    rendered = Redcarpet.new(text, *options).to_html.html_safe
+    rendered = renderer.render(text).html_safe
     "<div class='markdown'>#{rendered}</div>".html_safe
   end
 
