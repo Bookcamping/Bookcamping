32a11825c443c2a90d8a27d2236dfc6da8054646
danigb <danigb@gmail.com>
2012-09-02 12:42:54 +0200

    Better redirect using regex

--- app/controllers/blog_redirect_controller.rb
+++ app/controllers/blog_redirect_controller.rb
@@ -4,8 +4,20 @@ class BlogRedirectController < ApplicationController
   end
 
   def show
-    post_id = BLOG_REDIRECTS.fetch(params[:id])
-    url = post_id ? "post/#{post_id}/#{params[:id]}" : ""
-    redirect_to "http://blog.bookcamping.cc/#{url}"
+    if post_id
+      url = "post/#{post_id}/#{params[:id]}"
+      redirect_to "http://blog.bookcamping.cc/#{url}"
+    else
+      redirect_to 'http://blog.bookcamping.cc'
+    end
+  end
+
+  protected
+  def post_id
+    BLOG_REDIRECTS_REGEXP.keys.each do |regexp|
+      puts "TEST #{regexp}"
+      return BLOG_REDIRECTS_REGEXP[regexp] if regexp =~ params[:id]
+    end
+    nil
   end
 end
--- config/initializers/blog_redirect.rb
+++ config/initializers/blog_redirect.rb
@@ -1,8 +1,13 @@
 
 
-BLOG_REDIRECTS = {}
+BLOG_REDIRECTS_IDS = {}
+BLOG_REDIRECTS_SLUG = {}
+BLOG_REDIRECTS_REGEXP = {}
 File.open("#{Rails.root}/config/blog_migration.txt").each do |line|
-  s, d = line.split
-  BLOG_REDIRECTS.store(s[1..-1], d) if s
+  i, s, d = line.split
+  BLOG_REDIRECTS_IDS.store(i, d) if s
+  BLOG_REDIRECTS_SLUG.store(s[1..-1], d) if s
+  regexp = Regexp.new("(^#{i}$|^#{i}-|#{s})")
+  BLOG_REDIRECTS_REGEXP.store(regexp, d) if s
 end
 
